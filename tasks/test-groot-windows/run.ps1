4c5458ea8e043250b752eb28eee23619bb2330d8
Sam Smith <sesmith177@gmail.com>
2018-01-29 18:13:01 -0500

    add groot-windows
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ tasks/test-groot-windows/run.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+go.exe version
+
+$env:GOPATH = $PWD
+$env:PATH="$env:GOPATH\bin;" +$env:PATH
+
+cd $env:GOPATH/src/code.cloudfoundry.org/groot-windows
+
+Write-Host "Installing Ginkgo"
+go.exe install ./vendor/github.com/onsi/ginkgo/ginkgo
+if ($LastExitCode -ne 0) {
+    throw "Ginkgo installation process returned error code: $LastExitCode"
+}
+
+ginkgo.exe -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10
+$exitCode = $LastExitCode
+
+Exit $exitCode
