d4d08941a5153418394325fb66f9cb9e43576abf
Sam Smith <sesmith177@gmail.com>
2018-02-22 17:52:30 -0500

    Actually add the task

---
+++ tasks/build-groot/run.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:PATH = "$env:GOPATH\bin;" + $env:PATH
+
+go.exe version
+
+pushd groot
+  go build -o ..\groot-binary\groot.exe main.go
+
+  gcc.exe -c ".\volume\quota\quota.c" -o "$env:TEMP\quota.o"
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+  gcc.exe -shared -o "..\groot-binary\quota.dll" "$env:TEMP\quota.o" -lole32 -loleaut32
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+popd
