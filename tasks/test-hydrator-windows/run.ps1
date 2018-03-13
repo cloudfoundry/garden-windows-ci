ca7e6220f0f130d581e1b9ba35bf7c048316675b
Natalie Arellano <narellano@pivotal.io>
2018-03-13 15:45:31 -0400

    Add a tasks for running hydrator unit tests.
    [#155477778]
    
    Signed-off-by: Andrew Keesler <akeesler@pivotal.io>

---
+++ tasks/test-hydrator-windows/run.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:GOPATH=$PWD
+
+go get github.com/onsi/ginkgo/ginkgo
+
+& "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -keepGoing -randomizeSuites src/code.cloudfoundry.org/hydrator
+Exit $LastExitCode
