c1cec18247c224b00d2d73991766e7513e99c416
Sam Smith <sesmith177@gmail.com>
2018-04-12 14:53:46 -0400

    Add containerd pipeline
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ tasks/test-containerd/run.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:GOPATH=$PWD
+
+pushd src/github.com/containerd/containerd
+  make test
+  if ($LastExitCode -ne 0) {
+      throw "'make test' failed with exit code: $LastExitCode"
+  }
+
+  make root-test
+  if ($LastExitCode -ne 0) {
+      throw "'make root-test' failed with exit code: $LastExitCode"
+  }
+
+  make integration CONTAINERD_RUNTIME=io.containerd.runtime.v1.winc
+  if ($LastExitCode -ne 0) {
+      throw "'make integration' failed with exit code: $LastExitCode"
+  }
+popd
