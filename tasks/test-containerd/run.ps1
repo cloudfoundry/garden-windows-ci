65d4ea55395cbd0abe512d7f4e1e76e5d203238c
Sunjay Bhatia <sbhatia@pivotal.io>
2018-04-12 15:35:28 -0400

    Fixup containerd job
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- tasks/test-containerd/run.ps1
+++ tasks/test-containerd/run.ps1
@@ -3,6 +3,14 @@ trap { $host.SetShouldExit(1) }
 
 $env:GOPATH=$PWD
 
+# need to create a gopath on our own. If we just use the
+# 'path:' field in task.yml, symlinks will be created and
+# go list ./... (called in the makefile) will complain
+
+mkdir src/github.com/containerd/containerd
+
+cp -r containerd-fork/* src/github.com/containerd/containerd
+
 pushd src/github.com/containerd/containerd
   make test
   if ($LastExitCode -ne 0) {
