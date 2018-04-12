eb0332b039c31357d61753fd7e2d48f329f9062e
Sam Smith <sesmith177@gmail.com>
2018-04-12 15:46:32 -0400

    Need to build containerd.exe by hand
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- tasks/test-containerd/run.ps1
+++ tasks/test-containerd/run.ps1
@@ -1,6 +1,8 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+go version
+
 $env:GOPATH=$PWD
 
 # need to create a gopath on our own. If we just use the
@@ -22,8 +24,15 @@ pushd src/github.com/containerd/containerd
       throw "'make root-test' failed with exit code: $LastExitCode"
   }
 
+  mkdir bin
+  $env:PATH+=";$PWD/bin"
+  go build -o bin/containerd.exe cmd/containerd
+  if ($LastExitCode -ne 0) {
+      throw "couldn't build containerd.exe: exit code $LastExitCode"
+  }
+
   make integration CONTAINERD_RUNTIME=io.containerd.runtime.v1.winc
   if ($LastExitCode -ne 0) {
-      throw "'make integration' failed with exit code: $LastExitCode"
+      throw "'make integration CONTAINERD_RUNTIME=io.containerd.runtime.v1.winc' failed with exit code: $LastExitCode"
   }
 popd
