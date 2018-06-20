5aa6881d275068d18cfc8ee4acd41b381071b675
Natalie Arellano <narellano@pivotal.io>
2018-06-20 17:20:14 -0400

    Remove create-release binaries
    - They aren't used, we can just create them on the fly in create-release scripts.
    - create-release scripts don't need conditional logic based on DEV_ENV, we can
      remove the tasks that try to exercise code branches.
    
    [#158500320]

--- tasks/create-fs-release-windows/run.ps1
+++ tasks/create-fs-release-windows/run.ps1
@@ -4,9 +4,6 @@ trap { $host.SetShouldExit(1) }
 # get tar on the path
 $env:PATH="$env:PATH;C:\var\vcap\bosh\bin"
 
-$timestamp=(cat create-timestamp/create-*-timestamp)
-$sha=(cat sha/create-*-windows-amd64.exe.sha256)
-
 push-location windows2016fs-release
   git config core.filemode false
   if ($LastExitCode -ne 0) {
@@ -15,28 +12,6 @@ push-location windows2016fs-release
   git submodule foreach --recursive git config core.filemode false
   if ($LastExitCode -ne 0) {
     exit $LastExitCode
-  }
-
-  if ($env:DEV_ENV -eq $null -or $env:DEV_ENV -eq "") {
-    set-content -path VERSION -value $timestamp -NoNewLine
-    set-content -path CREATE_BIN_SHA_WINDOWS -value $sha
-
-    git config --global user.email "pivotal-netgarden-eng@pivotal.io"
-    if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-    }
-    git config --global user.name "Greenhouse CI"
-    if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-    }
-    git add VERSION CREATE_BIN_SHA_WINDOWS
-    if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-    }
-    git commit -m "WIP - test"
-    if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-    }
   }
 
   new-item -type directory -force .\new-dir
