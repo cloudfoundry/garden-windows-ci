dbe3397586211525e4ab91d4a2cd6b6f024e65f7
Sam Smith <sesmith177@gmail.com>
2018-08-14 13:52:48 -0400

    Add job to test Envoy on Windows
    [#159366810]

---
+++ tasks/envoy-ci-windows/run.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:BAZEL_SH="c:\var\vcap\packages\msys2\usr\bin\bash.exe"
+$env:BAZEL_VC="c:\var\vcap\packages\vs_buildtools\VC"
+
+cd envoy
+powershell "./ci/do_ci.ps1"
+exit $LASTEXITCODE
