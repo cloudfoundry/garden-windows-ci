47775384822b78eda3648e4829ebe4a74bfef23e
Sam Smith <sesmith177@gmail.com>
2018-08-14 14:01:57 -0400

    Set ENVOY_BAZEL_ROOT in windows task

--- tasks/envoy-ci-windows/run.ps1
+++ tasks/envoy-ci-windows/run.ps1
@@ -3,7 +3,21 @@ trap { $host.SetShouldExit(1) }
 
 $env:BAZEL_SH="c:\var\vcap\packages\msys2\usr\bin\bash.exe"
 $env:BAZEL_VC="c:\var\vcap\packages\vs_buildtools\VC"
+$env:ENVOY_BAZEL_ROOT="c:\_eb"
+
+cmd.exe /c rd /s /q $env:ENVOY_BAZEL_ROOT
 
 cd envoy
 powershell "./ci/do_ci.ps1"
-exit $LASTEXITCODE
+$ec = $LASTEXITCODE
+if ($ec -ne 0) {
+  Write-Host "ci failed"
+  exit $ec
+}
+
+cmd.exe /c rd /s /q $env:ENVOY_BAZEL_ROOT
+$ec = $LASTEXITCODE
+if ($ec -ne 0) {
+  Write-Host "failed to clean up $env:ENVOY_BAZEL_ROOT"
+}
+exit $ec
