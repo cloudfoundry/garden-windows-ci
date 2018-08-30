579c1eae3fd50bf0f4529783fc3a6827c3360672
Sam Smith <sesmith177@gmail.com>
2018-08-30 10:12:36 -0400

    Remove msys sh from tar build PATH
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

--- tasks/build-tar/run.ps1
+++ tasks/build-tar/run.ps1
@@ -3,6 +3,7 @@ trap { $host.SetShouldExit(1) }
 
 # remove sh.exe from $env:PATH
 $env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -ne 'c:\var\vcap\packages\git\bin' }) -join ';'
+$env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -ne 'C:\var\vcap\packages\msys2\usr\bin' }) -join ';'
 
 bsdtar/install.ps1
 Exit $LastExitCode
