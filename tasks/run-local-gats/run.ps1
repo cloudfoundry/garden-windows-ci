852cf0c294da2472622ba8518ddd42a4c3b35495
Yechiel Kalmenson <ykalmenson@pivotal.io>
2018-11-20 16:14:01 -0500

    Wrap the TMP variable in quotes
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

--- tasks/run-local-gats/run.ps1
+++ tasks/run-local-gats/run.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-mkdir $env:TMP -ea 0
+mkdir "$env:TMP" -ea 0
 
 function Kill-Garden {
   Get-Process | foreach { if ($_.name -eq "gdn") { kill -Force $_.Id } }
