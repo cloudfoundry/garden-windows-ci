b5470ca46035ffcbf6e8a1b41e47430bcb4deb2c
Natalie Arellano <narellano@pivotal.io>
2018-12-10 17:16:17 -0500

    Turn on TLS 1.0 for downloading url rewrite module

--- tasks/docker-build-and-push/run.ps1
+++ tasks/docker-build-and-push/run.ps1
@@ -22,6 +22,13 @@ mkdir buildDir
 cp $env:DOCKERFILE buildDir\Dockerfile
 cp git-setup\Git-*-64-bit.exe buildDir\
 cp tar\tar-*.exe buildDir\
+
+# download.microsoft.com requires TLS 1.0 (it is disabled by default in the stemcell).
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Value 1 -Name 'Enabled' -Type DWORD
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Value 0 -Name 'DisabledByDefault' -Type DWORD
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Value 1 -Name 'Enabled' -Type DWORD
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Value 0 -Name 'DisabledByDefault' -Type DWORD
+
 curl -UseBasicParsing -Outfile buildDir\rewrite.msi -Uri "https://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi"
 curl -UseBasicParsing -Outfile buildDir\vc_redist.x64.exe -Uri "https://aka.ms/vs/15/release/vc_redist.x64.exe"
 cd buildDir
