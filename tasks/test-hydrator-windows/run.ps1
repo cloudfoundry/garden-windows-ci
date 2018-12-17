d4315d95c96a4d43b2cc12476a819b0ce7375bf1
Sam Smith <sesmith177@gmail.com>
2018-12-17 16:38:52 -0500

    Build add-layer dependencies for test-hydrator-windows
    Signed-off-by: Yechiel Kalmenson <ykalmenson@pivotal.io>

--- tasks/test-hydrator-windows/run.ps1
+++ tasks/test-hydrator-windows/run.ps1
@@ -5,5 +5,13 @@ $env:GOPATH=$PWD
 
 go get github.com/onsi/ginkgo/ginkgo
 
+mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
+$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
+
+$env:WINC_BINARY = "$PWD\winc-binary\winc.exe"
+$env:GROOT_BINARY = "$PWD\groot-binary\groot.exe"
+$env:DIFF_EXPORTER_BINARY = "$PWD\diff-exporter-binary\diff-exporter.exe"
+$env:GROOT_IMAGE_STORE="$env:EPHEMERAL_DISK_TEMP_PATH\groot"
+
 & "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -keepGoing -randomizeSuites src/code.cloudfoundry.org/hydrator
 Exit $LastExitCode
