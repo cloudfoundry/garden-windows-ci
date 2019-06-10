1a71eb6378968106a5eeb5d90ca034f491ee9cf8
Daniel Basner <dbasner@pivotal.io>
2019-06-10 10:38:22 -0400

    Adding resource for automatically updating vcredist.- Use new resource type that can run arbitrary commands to curl for updates- Upgraded url to use the latest release, 2019 instead of 2017.
    Signed-off-by: Micah Young <myoung@pivotal.io>

--- tasks/docker-build-and-push/run.ps1
+++ tasks/docker-build-and-push/run.ps1
@@ -23,6 +23,7 @@ mkdir buildDir
 cp $env:DOCKERFILE buildDir\Dockerfile
 cp git-setup\Git-*-64-bit.exe buildDir\
 cp tar\tar-*.exe buildDir\
+cp vcredist\vc_redist.x64.exe buildDir\
 
 # download.microsoft.com requires TLS 1.0 (it is disabled by default in the stemcell).
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client" -Value 1 -Name 'Enabled' -Type DWORD
@@ -31,7 +32,7 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server" -Value 0 -Name 'DisabledByDefault' -Type DWORD
 
 curl -UseBasicParsing -Outfile buildDir\rewrite.msi -Uri "https://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi"
-curl -UseBasicParsing -Outfile buildDir\vc_redist.x64.exe -Uri "https://aka.ms/vs/15/release/vc_redist.x64.exe"
+
 cd buildDir
 
 Run-Docker "--version"
