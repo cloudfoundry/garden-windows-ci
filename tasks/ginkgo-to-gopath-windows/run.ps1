7acad4f778e5f1d9ad3f79eabd394622712fe3a0
Arjun Sreedharan <asreedharan@pivotal.io>
2019-12-03 13:48:50 -0500

    USERPROFILE is home for windows in ginkgo exe
    Signed-off-by: Jack Feeny <jfeeny@pivotal.io>

--- tasks/ginkgo-to-gopath-windows/run.ps1
+++ tasks/ginkgo-to-gopath-windows/run.ps1
@@ -4,6 +4,7 @@ trap { $host.SetShouldExit(1) }
 mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
 $env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
 $env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
+$env:USERPROFILE = "$env:EPHEMERAL_DISK_TEMP_PATH"
 
 function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
