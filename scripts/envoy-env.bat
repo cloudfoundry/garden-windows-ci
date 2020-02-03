6dc082831ad8d8d789b4a5e9a2487b4b6fa79f14
Sunjay Bhatia <sbhatia@pivotal.io>
2020-02-03 16:50:14 -0500

    Fix envoy dev-box manifest and add script for envoy env variables
    Co-authored-by: William A Rowe Jr <wrowe@pivotal.io>

---
+++ scripts/envoy-env.bat
@@ -0,0 +1,9 @@
+rem set BAZEL_SH=C:\var\vcap\packages\msys2\usr\bin\bash.exe
+rem set BAZEL_VC=C:\var\vcap\data\VSBuildTools\2019\VC
+rem set BAZEL_LLVM=c:\var\vcap\data\llvm
+rem set USE_CLANG_CL=1
+set PATH=C:\var\vcap\packages\cmake\bin;C:\var\vcap\packages\ninja;C:\var\vcap\packages\python3;C:\var\vcap\data\VSBuildTools\2019\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64;C:\var\vcap\data\llvm\bin;C:\var\vcap\packages\msys2\usr\bin;C:\Windows\System32;C:\Windows;C:\windows\System32\WindowsPowerShell\v1.0;C:\var\vcap\packages\git\bin;C:\var\vcap\packages\bazel
+set MSYS2_ARG_CONV_EXCL=*
+set USERNAME=pivotal
+set USER=pivotal
+set HOME=c:\u\p
