448c3a33073e502e3e01399800584568f1036b63
Jack Feeny <jfeeny@pivotal.io>
2020-02-27 12:08:36 -0500

    remove unnecessary env var

--- tasks/generate-rootfs-release-notes/run.ps1
+++ tasks/generate-rootfs-release-notes/run.ps1
@@ -15,7 +15,6 @@ mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
 $env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
 $env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
 $env:USERPROFILE = "$env:EPHEMERAL_DISK_TEMP_PATH"
-$repoPath = (Resolve-Path repo).Path
 
 restart-service docker
 
