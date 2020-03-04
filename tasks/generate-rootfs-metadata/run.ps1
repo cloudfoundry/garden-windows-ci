6b5dd5852fea208a722b3b4a1a2685145794b9f0
Jack Feeny <jfeeny@pivotal.io>
2020-03-04 10:49:38 -0500

    Generate release notes at the ship jobs
    * generating release notes prior to the ship step will
      result in release notes incorrectly omitting reference to
      KBs that were in builds that we didn't ship

---
+++ tasks/generate-rootfs-metadata/run.ps1
@@ -0,0 +1,36 @@
+﻿$ProgressPreference="SilentlyContinue"
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+function Run-Docker {
+  param([String[]] $cmd)
+
+  docker @cmd
+  if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+  }
+}
+
+mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
+$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
+$env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
+$env:USERPROFILE = "$env:EPHEMERAL_DISK_TEMP_PATH"
+
+restart-service docker
+
+$version=(cat image-version/version)
+Run-Docker "--version"
+$releaseNotesDir = "$PWD\notes"
+$notesFile = "rootfs-metadata-$version"
+
+$kbs = Run-Docker "run", "${env:IMAGE_NAME}:$version", "powershell", "(get-hotfix).HotFixID"
+Write-Output "kbs in image: " + $kbs
+
+$releaseMetadata = @{}
+$releaseMetadata["kbs"] = $kbs
+$releaseMetadata["version"] = $version
+
+
+$releaseMetadata | convertto-json |  Out-file -FilePath $releaseNotesDir/$notesFile
+
+
