e8a8d5b9cb0c63f99fc250dc6ac081b2d3b10e7c
Anthony Emengo <anthonyemengojr@gmail.com>
2020-05-22 11:20:51 -0400

    Add revamped kb-metadata generation
    [#172003250]

---
+++ tasks/generate-rootfs-metadata-2/run.ps1
@@ -0,0 +1,21 @@
+﻿$ProgressPreference="SilentlyContinue"
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$version=(cat image-version/version)
+
+docker run `
+  -v "$PWD\artifacts:c:\artifacts" `
+  -w c:\artifacts `
+  --rm `
+  -it `
+  "$env:IMAGE_NAME:$version" `
+  "powershell" "-Command" "Get-Hotfix | Format-Table -Auto > kb-metadata"
+if ($LASTEXITCODE -ne 0) {
+  Exit $LASTEXITCODE
+}
+
+Write-Output "$env:IMAGE_NAME:$version"
+Write-Output $output
+
+
