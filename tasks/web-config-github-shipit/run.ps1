83cc96d5580822ee2689c922b4da2c25075c5765
Anthony Emengo <anthonyemengojr@gmail.com>
2020-06-26 11:36:30 -0400

    Fixes web-config-buildpack ship-it job

--- tasks/web-config-github-shipit/run.ps1
+++ tasks/web-config-github-shipit/run.ps1
@@ -2,6 +2,8 @@
 trap { $host.SetShouldExit(1) }
 
 Push-Location web-config-buildpack
+  git checkout "$env:BRANCH"
+
   .\build.ps1 --configuration Release --git-hub-token "$env:GITHUB_AUTH_TOKEN" --target Release --stack windows
   if ($LASTEXITCODE -ne 0) {
     Exit $LASTEXITCODE
