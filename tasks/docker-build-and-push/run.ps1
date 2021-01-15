5c0f2c55fa70b02cb9d09ac450648b5c32eaafce
Anthony Emengo <aemengo@vmware.com>
2021-01-15 14:40:14 -0500

    Fix again
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/docker-build-and-push/run.ps1
+++ tasks/docker-build-and-push/run.ps1
@@ -56,7 +56,7 @@ $env:TEST_CANDIDATE_IMAGE=$env:IMAGE_NAME
 $env:VERSION_TAG=$env:OS_VERSION
 
 go get -u github.com/onsi/ginkgo/ginkgo
-& $env:HOME\go\bin\ginkgo.exe -v $repoPath
+& $env:USERPROFILE\go\bin\ginkgo.exe -v $repoPath
 if ($LASTEXITCODE -ne 0) {
   Exit $LASTEXITCODE
 }
