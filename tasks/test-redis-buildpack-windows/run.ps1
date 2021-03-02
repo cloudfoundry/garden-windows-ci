15128ede41bf25373c3ac6fcb2b92ae6a97b8410
Victoria Henry <vhenry@vmware.com>
2021-03-01 20:46:37 -0500

    Correct command to install dotnet script

--- tasks/test-redis-buildpack-windows/run.ps1
+++ tasks/test-redis-buildpack-windows/run.ps1
@@ -4,8 +4,8 @@ trap { $host.SetShouldExit(1) }
 cd redis-buildpack
 git checkout "$env:BRANCH"
 
-Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -Proxy $env:HTTP_PROXY -ProxyUseDefaultCredentials -OutFile 'dotnet-install.ps1';
-./dotnet-install.ps1 -InstallDir '~/.dotnet' -Version '3.1.201' -ProxyAddress $env:HTTP_PROXY -ProxyUseDefaultCredentials;
+Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'dotnet-install.ps1';
+./dotnet-install.ps1 -InstallDir '~/.dotnet' -Version '3.1.201' ;
 
 ./build.ps1 Test --stack Windows
 exit $LASTEXITCODE
