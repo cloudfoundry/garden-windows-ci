1b8ae6f28384c3f7976258cf943e00f3dff66e67
Anthony Emengo <aemengo@vmware.com>
2021-05-24 17:00:32 -0400

    Update test-groot-windows
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/test-groot-windows/run.ps1
+++ tasks/test-groot-windows/run.ps1
@@ -3,14 +3,7 @@ trap { $host.SetShouldExit(1) }
 
 go.exe version
 
-mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
-$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
-$env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
-
-$env:GOPATH = $PWD
-$env:PATH="$env:GOPATH\bin;" +$env:PATH
-
-cd $env:GOPATH/src/code.cloudfoundry.org/groot-windows
+cd groot-windows
 
 Write-Host "Installing Ginkgo"
 go.exe get -u github.com/onsi/ginkgo/ginkgo
