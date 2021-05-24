0ce67e1fcb3bc422be6cceb94028ae99869bf939
Anthony Emengo <aemengo@vmware.com>
2021-05-24 18:39:10 -0400

    Update test-nstar
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

---
+++ tasks/ginkgo-test-windows/run.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+go.exe version
+
+cd $env:TEST_PATH
+
+Write-Host "Installing Ginkgo"
+go.exe get -u github.com/onsi/ginkgo/ginkgo
+go.exe get -u github.com/onsi/gomega/...
+if ($LastExitCode -ne 0) {
+    throw "Ginkgo installation process returned error code: $LastExitCode"
+}
+
+~/go/bin/ginkgo.exe -nodes $env:NODES -r -race -keepGoing -randomizeSuites
+if ($LastExitCode -ne 0) {
+    throw "Testing $env:TEST_PATH returned error code: $LastExitCode"
+}
+
+
+Exit $exitCode
