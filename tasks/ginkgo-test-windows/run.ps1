8e03ab4ac27e21658cb8fbab82a9d17310f6c0b6
Anthony Emengo <aemengo@vmware.com>
2021-05-24 18:51:19 -0400

    fixup
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/ginkgo-test-windows/run.ps1
+++ tasks/ginkgo-test-windows/run.ps1
@@ -6,9 +6,3 @@ go.exe version
 cd $env:TEST_PATH
 
 ~/go/bin/ginkgo.exe -nodes $env:NODES -r -race -keepGoing -randomizeSuites
-if ($LastExitCode -ne 0) {
-    throw "Testing $env:TEST_PATH returned error code: $LastExitCode"
-}
-
-
-Exit $exitCode
