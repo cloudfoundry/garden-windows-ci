74338f3daa860740a9c76a73dbec7bc08f07e85f
Anthony Emengo <aemengo@vmware.com>
2021-05-26 13:45:28 -0400

    fixes
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/test-winc/run.ps1
+++ tasks/test-winc/run.ps1
@@ -1,25 +1,6 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-function get-firewall {
-	param([string] $profile)
-	$firewall = (Get-NetFirewallProfile -Name $profile)
-	$result = "{0},{1},{2}" -f $profile,$firewall.DefaultInboundAction,$firewall.DefaultOutboundAction
-	return $result
-}
-
-function check-firewall {
-	param([string] $profile)
-	$firewall = (get-firewall $profile)
-	Write-Host $firewall
-	if ($firewall -ne "$profile,Block,Block") {
-		Write-Host $firewall
-		Write-Error "Unable to set $profile Profile"
-	}
-}
-
-Set-NetFirewallProfile -All -DefaultInboundAction Block -DefaultOutboundAction Allow -Enabled True
-
 $ephemeral_disk_temp_path="C:\var\vcap\data\tmp"
 mkdir "$ephemeral_disk_temp_path" -ea 0
 $env:TEMP = $env:TMP = $ephemeral_disk_temp_path
@@ -33,17 +14,9 @@ $env:GROOT_BINARY = "$PWD\groot-binary\groot.exe"
 
 cd winc
 
-go build -o winc-network.exe ./cmd/winc-network
+go build -o winc-network.exe -tags "hnsAcls" ./cmd/winc-network
 if ($LastExitCode -ne 0) {
     throw "Building winc-network failed with exit code: $LastExitCode"
-}
-gcc.exe -c src/code.cloudfoundry.org/winc/network/firewall/dll/firewall.c -o firewall.o
-if ($LastExitCode -ne 0) {
-    throw "Compiling firewall.o failed with exit code: $LastExitCode"
-}
-gcc.exe -shared -o firewall.dll firewall.o -lole32 -loleaut32
-if ($LastExitCode -ne 0) {
-    throw "Compiling firewall.dll failed with exit code: $LastExitCode"
 }
 
 $config = '{"name": "winc-nat"}'
