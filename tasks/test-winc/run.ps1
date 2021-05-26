f261ac5f7351ce2b77630513b0cdf708e0bc2f27
Anthony Emengo <aemengo@vmware.com>
2021-05-26 13:28:36 -0400

    Fix test-winc
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/test-winc/run.ps1
+++ tasks/test-winc/run.ps1
@@ -20,22 +20,20 @@ function check-firewall {
 
 Set-NetFirewallProfile -All -DefaultInboundAction Block -DefaultOutboundAction Allow -Enabled True
 
+$ephemeral_disk_temp_path="C:\var\vcap\data\tmp"
+mkdir "$ephemeral_disk_temp_path" -ea 0
+$env:TEMP = $env:TMP = $ephemeral_disk_temp_path
+$env:GROOT_IMAGE_STORE = "$ephemeral_disk_temp_path\groot"
+
 go.exe version
-
-$env:GOPATH = $PWD
-$env:PATH="$env:GOPATH\bin;" +$env:PATH
-
-mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
-$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
-$env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
-
-$env:GROOT_IMAGE_STORE = "$env:EPHEMERAL_DISK_TEMP_PATH\groot"
 
 $env:GROOT_BINARY = "$PWD\groot-binary\groot.exe"
 
 & "$env:GROOT_BINARY" --driver-store "$env:GROOT_IMAGE_STORE" pull "$env:WINC_TEST_ROOTFS"
 
-go build -o winc-network.exe code.cloudfoundry.org/winc/cmd/winc-network
+cd winc
+
+go build -o winc-network.exe ./cmd/winc-network
 if ($LastExitCode -ne 0) {
     throw "Building winc-network failed with exit code: $LastExitCode"
 }
@@ -55,24 +53,16 @@ if ($LastExitCode -ne 0) {
     throw "Running winc-network.exe --action delete failed with exit code: $LastExitCode"
 }
 
-cd $env:GOPATH/src/code.cloudfoundry.org/winc
-
-Write-Host "Installing Ginkgo"
-go.exe install ./vendor/github.com/onsi/ginkgo/ginkgo
-if ($LastExitCode -ne 0) {
-    throw "Ginkgo installation process returned error code: $LastExitCode"
-}
-
 # Turn on debug for winc integration
 $env:DEBUG="true"
 
-ginkgo.exe -p -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 -skipPackage winc-network,perf
+~/go/bin/ginkgo.exe -p -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 -skipPackage winc-network,perf
 $exitCode = $LastExitCode
 if ($exitCode -ne 0) {
   Exit $exitCode
 }
 
-ginkgo.exe -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 ./integration/perf  ./integration/winc-network
+~/go/bin/ginkgo.exe -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 ./integration/perf  ./integration/winc-network
 $exitCode = $LastExitCode
 if ($exitCode -ne 0) {
   Exit $exitCode
