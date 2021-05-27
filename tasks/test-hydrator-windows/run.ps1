7e80d21a1486535d2733f3c9b19ec8a5f9dbe4cf
Anthony Emengo <aemengo@vmware.com>
2021-05-27 12:05:49 -0400

    Fix test-hydrator
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/test-hydrator-windows/run.ps1
+++ tasks/test-hydrator-windows/run.ps1
@@ -1,18 +1,14 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-$env:GOPATH=$PWD
-
-go get github.com/onsi/ginkgo/ginkgo
-
-mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
-$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
-$env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
+$ephemeral_disk_temp_path="C:\var\vcap\data\tmp"
+mkdir "$ephemeral_disk_temp_path" -ea 0
+$env:TEMP = $env:TMP = $ephemeral_disk_temp_path
 
 $env:WINC_BINARY = "$PWD\winc-binary\winc.exe"
 $env:GROOT_BINARY = "$PWD\groot-binary\groot.exe"
 $env:DIFF_EXPORTER_BINARY = "$PWD\diff-exporter-binary\diff-exporter.exe"
-$env:GROOT_IMAGE_STORE="$env:EPHEMERAL_DISK_TEMP_PATH\groot-hydrator-tests"
+$env:GROOT_IMAGE_STORE = "$ephemeral_disk_temp_path\groot"
 
-& "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -keepGoing -randomizeSuites src/code.cloudfoundry.org/hydrator
+~/go/bin/ginkgo.exe -nodes $env:NODES -r -race -keepGoing -randomizeSuites src/code.cloudfoundry.org/hydrator
 Exit $LastExitCode
