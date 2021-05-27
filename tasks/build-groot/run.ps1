2790370d2fa7cdcdd7f932ec3b762e4e892976c7
Anthony Emengo <aemengo@vmware.com>
2021-05-27 10:46:32 -0400

    Fixes run-gats
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- tasks/build-groot/run.ps1
+++ tasks/build-groot/run.ps1
@@ -8,17 +8,19 @@ go.exe version
 $binaryDir = "$PWD\groot-binary"
 $binaryDir = updateDirIfSymlink "$binaryDir"
 
-pushd src\code.cloudfoundry.org\groot-windows
-  go build -o "$binaryDir\groot.exe" main.go
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
-  gcc.exe -c ".\volume\quota\quota.c" -o "$env:TEMP\quota.o"
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
-  gcc.exe -shared -o "$binaryDir\quota.dll" "$env:TEMP\quota.o" -lole32 -loleaut32
-  if ($LastExitCode -ne 0) {
-    exit $LastExitCode
-  }
-popd
+cd groot-windows
+
+go build -o "$binaryDir\groot.exe" main.go
+if ($LastExitCode -ne 0) {
+  exit $LastExitCode
+}
+
+gcc.exe -c ".\volume\quota\quota.c" -o "$env:TEMP\quota.o"
+if ($LastExitCode -ne 0) {
+  exit $LastExitCode
+}
+
+gcc.exe -shared -o "$binaryDir\quota.dll" "$env:TEMP\quota.o" -lole32 -loleaut32
+if ($LastExitCode -ne 0) {
+  exit $LastExitCode
+}
--- tasks/build-winc-network/run.ps1
+++ tasks/build-winc-network/run.ps1
@@ -5,15 +5,12 @@ trap { $host.SetShouldExit(1) }
 
 go.exe version
 
-mkdir "$env:EPHEMERAL_DISK_TEMP_PATH" -ea 0
-$env:TEMP = $env:TMP = $env:GOTMPDIR = $env:EPHEMERAL_DISK_TEMP_PATH
-$env:GOCACHE = "$env:EPHEMERAL_DISK_TEMP_PATH\go-build"
-
-$env:GOPATH=$PWD
 $binaryDir = "$PWD\winc-network-binary"
-
 $binaryDir = updateDirIfSymlink "$binaryDir"
 
-pushd src\code.cloudfoundry.org\winc
-	go build -o "$binaryDir\winc-network.exe" -tags "hnsAcls" .\cmd\winc-network
-popd
+cd winc
+
+go.exe build -o "$binaryDir\winc-network.exe" -tags "hnsAcls" .\cmd\winc-network
+if ($LastExitCode -ne 0) {
+	exit $LastExitCode
+}
