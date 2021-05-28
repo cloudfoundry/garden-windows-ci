43dedfb269323394046e85ccaa0eb3e496a96a03
Anthony Emengo <aemengo@vmware.com>
2021-05-28 08:47:04 -0400

    Fix winc networking tests
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

---
+++ tasks/test-winc-wip/run.ps1
@@ -0,0 +1,42 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$ephemeral_disk_temp_path="C:\var\vcap\data\tmp"
+mkdir "$ephemeral_disk_temp_path" -ea 0
+$env:TEMP = $env:TMP = $ephemeral_disk_temp_path
+$env:GROOT_IMAGE_STORE = "$ephemeral_disk_temp_path\groot"
+
+go.exe version
+
+$env:GROOT_BINARY = "$PWD\groot-binary\groot.exe"
+
+& "$env:GROOT_BINARY" --driver-store "$env:GROOT_IMAGE_STORE" pull "$env:WINC_TEST_ROOTFS"
+
+cd winc
+
+go build -o winc-network.exe -tags "hnsAcls" ./cmd/winc-network
+if ($LastExitCode -ne 0) {
+    throw "Building winc-network failed with exit code: $LastExitCode"
+}
+
+$config = '{"name": "winc-nat"}'
+set-content -path "$env:TEMP\interface.json" -value $config
+./winc-network.exe --action delete --configFile "$env:TEMP/interface.json"
+if ($LastExitCode -ne 0) {
+    throw "Running winc-network.exe --action delete failed with exit code: $LastExitCode"
+}
+
+# Turn on debug for winc integration
+$env:DEBUG="true"
+
+~/go/bin/ginkgo.exe -p -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 -skipPackage winc-network,perf
+$exitCode = $LastExitCode
+if ($exitCode -ne 0) {
+  Exit $exitCode
+}
+
+~/go/bin/ginkgo.exe -r -race -keepGoing -randomizeSuites -failOnPending -slowSpecThreshold 10 ./integration/perf  ./integration/winc-network
+$exitCode = $LastExitCode
+if ($exitCode -ne 0) {
+  Exit $exitCode
+}
