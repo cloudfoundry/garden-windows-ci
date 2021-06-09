6baebbf1f481fc87862be87ddae4adedc56c6d91
Anthony Emengo <aemengo@vmware.com>
2021-06-09 16:12:09 -0400

    Add install ginkgo job
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

---
+++ tasks/install-ginkgo-windows/run.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+Write-Host "Installing Ginkgo"
+go.exe get -u github.com/onsi/ginkgo/ginkgo
+go.exe get -u github.com/onsi/gomega/...
+if ($LastExitCode -ne 0) {
+    throw "Ginkgo installation process returned error code: $LastExitCode"
+}
+
+Exit 0
