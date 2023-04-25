790af953c15a1b849107137abaf0e363b121a879
David Sabeti <sabetid@vmware.com>
2023-04-25 23:22:04 +0000

    run-local-gats: add --flake-attempts 2

--- tasks/run-local-gats/run.ps1
+++ tasks/run-local-gats/run.ps1
@@ -109,7 +109,7 @@ push-location garden-runc-release
   $env:GROOT_IMAGE_STORE="$grootImageStore"
 
   Push-Location src/garden-integration-tests
-    go run github.com/onsi/ginkgo/v2/ginkgo -randomize-suites --succinct
+    go run github.com/onsi/ginkgo/v2/ginkgo -randomize-suites --succinct --flake-attempts 2
     $ExitCode="$LastExitCode"
   Pop-Location
 Pop-Location
